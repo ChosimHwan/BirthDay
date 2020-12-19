defc4fdb2a7bcdc9d06615747d3da038f322bcd3
root <root@chino4510.cafe24.com>
2020-12-19 15:36:25 +0900

    메인페이지 작업

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,98 +3,57 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
+            .body-color{
+                background-color:#aaf0d1;
             }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
+            .head-wrap{
                 text-align: center;
             }
-
-            .title {
-                font-size: 84px;
+            .navbar-wrap{
+                margin-top: 20px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
             }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
+            .navbar-wrap div{
+                width: 300px;
+                font-weight: bold;
+                font-size: 20px;
+                cursor: pointer;
             }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            .head-img{
+                max-width: 20%;
+            }
+            .item-top{
+                align-self: flex-start;
+            }
+            .item-end{
+                align-self: flex-end;
+            }
+            .img-wrap{
+                text-align: center;
+                margin-top: 50px;
+            }
+            .img-wrap img{
+                border-radius: 20px;
             }
         </style>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+    <body class="body-color">
+        <title>세은이의 생일을 축하합니다</title>
+        <section>
+            <div class="head-wrap">
+                <img class="head-img" src="/img/cake.png"/>
+                <div class="navbar-wrap">
+                    <div class="item-top">GIFT</div>
+                    <div>LETTER</div>
+                    <div class-="item-end">PICTURE</div>
                 </div>
             </div>
-        </div>
+            <div class="img-wrap">
+                <img src="/img/seun_main.jpg"/>
+            </div>
+        </section>
     </body>
 </html>
